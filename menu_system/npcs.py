97f159615c0f3a94a61a749dc37819ddc53b9c0d
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-04-01 16:21:59 -0300

    Adição de baús e melhorias de colisão de npcs, baus e correção da hitbox de colisao do player

--- menu_system/npcs.py
+++ menu_system/npcs.py
@@ -2,11 +2,12 @@ import pygame
 from dialogo import Dialogo
 
 class NPC(pygame.sprite.Sprite):
-    def __init__(self,image,screen):
+    def __init__(self,image,screen,pos_x,pos_y):
         super().__init__()  # Chama o inicializador da classe pai
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.topleft = (300,800)
+        self.rect.topleft = (pos_x,pos_y)
+        self.dialogo_rect = pygame.Rect(self.rect.left-32, self.rect.top-32, self.rect.width+64, self.rect.height+64)
         self.texto = {
             'personagem_1':'Omori',
             'texto_1':['Bem vindo ao Espaço em branco', 'Meu nome é Omori'],
